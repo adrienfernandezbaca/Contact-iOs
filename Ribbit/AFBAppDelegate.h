e349924b1b9900d383192d4d7881cff5ece537d6
adrienfernandezbaca <adrien.fernandez-baca@hec.edu>
2013-10-25 19:45:22 -0700

    Initial Commit

---
+++ Ribbit/AFBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AFBAppDelegate.h
+//  Ribbit
+//
+//  Created by Adrien Fernandez Baca on 25/10/2013.
+//  Copyright (c) 2013 Adrien Fernandez Baca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Adrien Fernandez Baca on 25/10/2013.
+//  Copyright (c) 2013 Adrien Fernandez Baca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AFBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AFBAppDelegate class]));
+    }
+}
